1aeb21fcddbb04b14ef290e56e88bcbdd3a1afc4
Vitor Freitas <vitorfs@gmail.com>
2014-06-02 09:19:14 -0300

    creating cover page and auth

---
+++ bootcamp/auth/forms.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+def ForbiddenUsernamesValidator(value):
+    forbidden_usernames = ['admin', 'settings', 'news', 'about', 'help', 'signin', 'signup', 
+        'signout', 'terms', 'privacy', 'cookie', 'new', 'login', 'logout', 'administrator', 
+        'join', 'account', 'username', 'root', 'blog', 'user', 'users', 'billing', 'subscribe',
+        'reviews', 'review', 'blog', 'blogs', 'edit', 'mail', 'email', 'home', 'job', 'jobs', 
+        'contribute', 'newsletter', 'shop', 'profile', 'register', 'auth', 'authentication',
+        'campaign', 'config', 'delete', 'remove', 'forum', 'forums', 'download', 'downloads', 
+        'contact', 'blogs', 'feed', 'feeds', 'faq', 'intranet', 'log', 'registration', 'search', 
+        'explore', 'rss', 'support', 'status', 'static', 'media', 'setting', 'css', 'js',
+        'follow', 'activity', 'questions', 'articles',]
+    if value.lower() in forbidden_usernames:
+        raise ValidationError('This is a reserved word.')
+
+def InvalidUsernameValidator(value):
+    if '@' in value or '+' in value or '-' in value:
+        raise ValidationError('Enter a valid username.')
+
+def UniqueEmailValidator(value):
+    if User.objects.filter(email__iexact=value).exists():
+        raise ValidationError('User with this Email already exists.')
+
+def UniqueUsernameIgnoreCaseValidator(value):
+    if User.objects.filter(username__iexact=value).exists():
+        raise ValidationError('User with this Username already exists.')
+
+class SignUpForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
+        max_length=30,
+        required=True,
+        help_text='Usernames may contain <strong>alphanumeric</strong>, <strong>_</strong> and <strong>.</strong> characters')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), 
+        label="Confirm your password",
+        required=True)
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}), 
+        required=True,
+        max_length=75)
+
+    class Meta:
+        model = User
+        exclude = ['last_login', 'date_joined']
+        fields = ['username', 'email', 'password', 'confirm_password',]
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].validators.append(ForbiddenUsernamesValidator)
+        self.fields['username'].validators.append(InvalidUsernameValidator)
+        self.fields['username'].validators.append(UniqueUsernameIgnoreCaseValidator)
+        self.fields['email'].validators.append(UniqueEmailValidator)
+
+    def clean(self):
+        super(SignUpForm, self).clean()
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password and password != confirm_password:
+            self._errors['password'] = self.error_class(['Passwords don\'t match'])
+        return self.cleaned_data
--- bootcamp/auth/views.py
+++ bootcamp/auth/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from bootcamp.feeds.views import feeds
 from django.contrib import messages
+from bootcamp.auth.forms import SignUpForm
+from django.contrib.auth.models import User
 
 def login(request):
     if request.user.is_authenticated():
@@ -32,4 +34,17 @@ def logout(request):
     return redirect('/')
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if not form.is_valid():
+            return render(request, 'auth/signup.html', {'form': form})
+        else:
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(username=username, password=password, email=email)
+            user = authenticate(username=username, password=password)
+            django_login(request, user)
+            return feeds(request)
+    else:
+        return render(request, 'auth/signup.html', {'form': SignUpForm()})
