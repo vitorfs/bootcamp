3d2f7ea24d72a0a43a98aecc271b39df0146c428
Vitor Freitas <vitorfs@gmail.com>
2014-06-02 17:38:03 -0300

    account settings implementation

--- bootcamp/core/views.py
+++ bootcamp/core/views.py
@@ -5,6 +5,8 @@ from bootcamp.feeds.models import Feed
 from bootcamp.feeds.views import FEEDS_NUM_PAGES
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from bootcamp.core.forms import ProfileForm, ChangePasswordForm
+from django.contrib import messages
 
 def home(request):
     if request.user.is_authenticated():
@@ -32,3 +34,39 @@ def profile(request, username):
         'from_feed': from_feed,
         'page': 1
         })
+
+@login_required
+def settings(request):
+    user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.profile.job_title = form.cleaned_data.get('job_title')
+            user.email = form.cleaned_data.get('email')
+            user.profile.url = form.cleaned_data.get('url')
+            user.profile.location = form.cleaned_data.get('location')
+            user.save()
+            messages.add_message(request, messages.SUCCESS, 'Your profile were successfully edited.')
+    else:
+        form = ProfileForm(instance=user, initial={
+            'job_title': user.profile.job_title,
+            'url': user.profile.url,
+            'location': user.profile.location
+            })
+    return render(request, 'core/settings.html', {'form':form})
+
+@login_required
+def password(request):
+    user = request.user
+    if request.method == 'POST':
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            new_password = form.cleaned_data.get('new_password')
+            user.set_password(new_password)
+            user.save()
+            messages.add_message(request, messages.SUCCESS, 'Your password were successfully changed.')
+    else:
+        form = ChangePasswordForm(instance=user)
+    return render(request, 'core/password.html', {'form':form})
