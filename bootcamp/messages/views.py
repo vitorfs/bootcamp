543a090fa891ae930b32d1683369f9f30ae3b35f
Vitor Freitas <vitorfs@gmail.com>
2014-06-11 18:26:48 -0300

    create first model

--- bootcamp/messages/views.py
+++ bootcamp/messages/views.py
@@ -6,15 +6,15 @@ from bootcamp.decorators import ajax_required
 
 @login_required
 def inbox(request):
-    return render(request, 'messages/inbox.html')
+    return render(request, 'messages/inbox.html', {'active': 'inbox'})
 
 @login_required
 def sent(request):
-    return render(request, 'messages/sent.html')
+    return render(request, 'messages/sent.html', {'active': 'sent'})
 
 @login_required
 def compose(request):
-    return render(request, 'messages/compose.html')
+    return render(request, 'messages/compose.html', {'active': 'compose'})
 
 @login_required
 @ajax_required
@@ -27,4 +27,4 @@ def reply(request):
 
 @login_required
 def message(request, id):
-    return render(request, 'messages/message.html')
+    return render(request, 'messages/message.html', {'active': 'inbox'})
--- bootcamp/search/models.py
+++ bootcamp/search/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    conversation_id = models.IntegerField()
+    inbox_user = models.ForeignKey(User, related_name='+')
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    message = models.TextField(max_length=4000, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    last_reply = models.DateTimeField(blank=True, null=True)
+    from_user = models.ForeignKey(User, related_name='+')
+    to_user = models.ForeignKey(User, related_name='+')
+    parent = models.ForeignKey('Message', null=True, blank=True, related_name='+')
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _('Message')
+        verbose_name_plural = _('Messages')
+        ordering = ('-date',)
+
+    def __unicode__(self):
+        return self.subject
+
+    def get_replys(self):
+        return Message.objects.filter(parent=self).order_by('date')
