4b51c7ecc228269724b9081242ab118b93e2ad08
tainarareis <tainara.s.r@hotmail.com>
2016-05-26 19:57:43 -0300

    Last unitary tests in Feed model. Close #5

--- bootcamp/feeds/tests.py
+++ bootcamp/feeds/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from bootcamp.feeds.models import Feed
 from django.contrib.auth.models import User
+from bootcamp.activities.models import Activity
 
 
 class FeedsTest(TestCase):
@@ -10,14 +11,16 @@ class FeedsTest(TestCase):
 
     def setUp(self):
         'Setup users'
-        User.objects.create(username="user1", password="password")
-        User.objects.create(username="user2", password="password")
+        User.objects.create(username="user1", password="password1")
+        User.objects.create(username="user2", password="password2")
+        User.objects.create(username="user3", password="password3")
         self.user1 = User.objects.get(username="user1")
         self.user2 = User.objects.get(username="user2")
+        self.user3 = User.objects.get(username="user3")
         'Setup feeds'
-        Feed.objects.create(post="My first post", likes=3, user=self.user1)
-        Feed.objects.create(post="Commenting feed1", likes=2, user=self.user2)
-        Feed.objects.create(post="Second comment in feed1", likes=4, user=self.user1)
+        Feed.objects.create(post="My first post", user=self.user1)
+        Feed.objects.create(post="Commenting feed1", user=self.user2)
+        Feed.objects.create(post="Second comment in feed1", user=self.user1)
         self.feed1 = Feed.objects.get(post="My first post")
         self.feed2 = Feed.objects.get(post="Commenting feed1")
         self.feed3 = Feed.objects.get(post="Second comment in feed1")
@@ -50,3 +53,20 @@ class FeedsTest(TestCase):
         comment2 = self.feed1.comment(self.feed3.user, self.feed3.post)
         comments_quantity = 2
         self.assertEqual(self.feed1.calculate_comments(), comments_quantity)
+
+    def test_calculate_likes(self):
+        like = Activity.objects.create(activity_type=Activity.LIKE, feed=self.feed1.id, user=self.feed1.user)
+        assert self.feed1.calculate_likes() == 1
+
+    def test_get_likes(self):
+        like = []
+        like.append(Activity.objects.create(activity_type=Activity.LIKE, feed=self.feed1.id, user=self.feed1.user))
+        self.assertItemsEqual(self.feed1.get_likes(), like)
+
+    def test_get_likers(self):
+        Activity.objects.create(activity_type=Activity.LIKE, feed=self.feed1.id, user=self.user2)
+        Activity.objects.create(activity_type=Activity.LIKE, feed=self.feed1.id, user=self.user3)
+        likers = []
+        likers.append(self.user2)
+        likers.append(self.user3)
+        self.assertItemsEqual(self.feed1.get_likers(), likers)
