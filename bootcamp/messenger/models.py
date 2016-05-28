b9f9651010f2f72c82b4e787c7b2941b0674e5d6
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-05-28 13:41:59 -0500

    Making the messenger app code more PEPS8 compliant

--- bootcamp/messenger/models.py
+++ bootcamp/messenger/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.db.models import Max
 
+
 class Message(models.Model):
     user = models.ForeignKey(User, related_name='+')
     message = models.TextField(max_length=1000, blank=True)
@@ -23,30 +24,37 @@ class Message(models.Model):
     @staticmethod
     def send_message(from_user, to_user, message):
         message = message[:1000]
-        current_user_message = Message(from_user=from_user, 
-            message=message, 
-            user=from_user, 
-            conversation=to_user,
-            is_read=True)
+        current_user_message = Message(from_user=from_user,
+                                       message=message,
+                                       user=from_user,
+                                       conversation=to_user,
+                                       is_read=True)
         current_user_message.save()
-        Message(from_user=from_user, 
-            conversation=from_user,
-            message=message, 
-            user=to_user).save()
+        Message(from_user=from_user,
+                conversation=from_user,
+                message=message,
+                user=to_user).save()
+
         return current_user_message
 
     @staticmethod
     def get_conversations(user):
-        conversations = Message.objects.filter(user=user).values('conversation').annotate(last=Max('date')).order_by('-last')
+        conversations = Message.objects.filter(
+            user=user).values('conversation').annotate(
+                last=Max('date')).order_by('-last')
         users = []
         for conversation in conversations:
             users.append({
                 'user': User.objects.get(pk=conversation['conversation']),
                 'last': conversation['last'],
-                'unread': Message.objects.filter(user=user, conversation__pk=conversation['conversation'], is_read=False).count(),
+                'unread': Message.objects.filter(user=user,
+                                                 conversation__pk=conversation[
+                                                    'conversation'],
+                                                 is_read=False).count(),
                 })
+
         return users
 
-    #Message.objects.filter(is_read=False, user__username='vitorfs').values('to_user').annotate(Count('to_user')).order_by()
-    #Message.objects.filter(user__username='vitorfs').values('to_user', 'last').annotate(last=Max('date')).order_by()
-    #Message.objects.filter(user__username='vitorfs').values('to_user').annotate(last=Max('date')).order_by('last')
+    # Message.objects.filter(is_read=False, user__username='vitorfs').values('to_user').annotate(Count('to_user')).order_by()
+    # Message.objects.filter(user__username='vitorfs').values('to_user', 'last').annotate(last=Max('date')).order_by()
+    # Message.objects.filter(user__username='vitorfs').values('to_user').annotate(last=Max('date')).order_by('last')
--- bootcamp/messenger/views.py
+++ bootcamp/messenger/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 import json
 from bootcamp.messenger.models import Message
 
+
 @login_required
 def inbox(request):
     conversations = Message.get_conversations(user=request.user)
@@ -15,31 +16,37 @@ def inbox(request):
     if conversations:
         conversation = conversations[0]
         active_conversation = conversation['user'].username
-        messages = Message.objects.filter(user=request.user, conversation=conversation['user'])
+        messages = Message.objects.filter(user=request.user,
+                                          conversation=conversation['user'])
         messages.update(is_read=True)
         for conversation in conversations:
             if conversation['user'].username == active_conversation:
                 conversation['unread'] = 0
+
     return render(request, 'messenger/inbox.html', {
         'messages': messages,
         'conversations': conversations,
         'active': active_conversation
         })
 
+
 @login_required
 def messages(request, username):
     conversations = Message.get_conversations(user=request.user)
     active_conversation = username
-    messages = Message.objects.filter(user=request.user, conversation__username=username)
+    messages = Message.objects.filter(user=request.user,
+                                      conversation__username=username)
     messages.update(is_read=True)
     for conversation in conversations:
         if conversation['user'].username == username:
             conversation['unread'] = 0
+
     return render(request, 'messenger/inbox.html', {
         'messages': messages,
         'conversations': conversations,
         'active': active_conversation
         })
+
 
 @login_required
 def new(request):
@@ -48,26 +55,36 @@ def new(request):
         to_user_username = request.POST.get('to')
         try:
             to_user = User.objects.get(username=to_user_username)
+
         except Exception, e:
             try:
-                to_user_username = to_user_username[to_user_username.rfind('(')+1:len(to_user_username)-1]
+                to_user_username = to_user_username[
+                    to_user_username.rfind('(')+1:len(to_user_username)-1]
                 to_user = User.objects.get(username=to_user_username)
+
             except Exception, e:
                 return redirect('/messages/new/')
+
         message = request.POST.get('message')
         if len(message.strip()) == 0:
             return redirect('/messages/new/')
+
         if from_user != to_user:
             Message.send_message(from_user, to_user, message)
+
         return redirect(u'/messages/{0}/'.format(to_user_username))
+
     else:
         conversations = Message.get_conversations(user=request.user)
-        return render(request, 'messenger/new.html', {'conversations': conversations})
+        return render(request, 'messenger/new.html',
+                      {'conversations': conversations})
+
 
 @login_required
 @ajax_required
 def delete(request):
     return HttpResponse()
+
 
 @login_required
 @ajax_required
@@ -81,10 +98,13 @@ def send(request):
             return HttpResponse()
         if from_user != to_user:
             msg = Message.send_message(from_user, to_user, message)
-            return render(request, 'messenger/includes/partial_message.html', {'message': msg})
+            return render(request, 'messenger/includes/partial_message.html',
+                          {'message': msg})
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
+
 
 @login_required
 @ajax_required
@@ -99,6 +119,7 @@ def users(request):
             dump.append(user.username)
     data = json.dumps(dump)
     return HttpResponse(data, content_type='application/json')
+
 
 @login_required
 @ajax_required
