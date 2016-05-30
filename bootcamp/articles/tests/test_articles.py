71bd20c28dd54715789fb7dbca4dc94c9080d905
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-05-30 08:05:10 -0500

    Small styling fixes to the tests on the articles app

--- bootcamp/articles/tests/test_articles.py
+++ bootcamp/articles/tests/test_articles.py
@@ -4,6 +4,7 @@ from bootcamp.articles.models import Article
 from bootcamp.authentication.models import Profile
 from django.contrib.auth.models import User
 
+
 class ArticleTest(TestCase):
     def setUp(self):
         self.client = Client()
@@ -11,10 +12,12 @@ class ArticleTest(TestCase):
 
     def test_validate_article_edition(self):
         c = Client()
-
-        user1 = User.objects.create_user(username="teste1234",email="reallynice@gmail.com", password="supersecret123")
-
-        user2 = User.objects.create_user(username="teste12345",email="reallynice2@gmail.com", password="supersecret123")
+        user1 = User.objects.create_user(username="teste1234",
+                                         email="reallynice@gmail.com",
+                                         password="supersecret123")
+        user2 = User.objects.create_user(username="teste12345",
+                                         email="reallynice2@gmail.com",
+                                         password="supersecret123")
 
         article = Article()
         article.title = "nicetitle"
@@ -22,17 +25,16 @@ class ArticleTest(TestCase):
         article.create_user = user2
         article.create_user.id = user2.id
         article.save()
-
         self.client.login(username="teste1234", password="supersecret123")
-        response = self.client.get(reverse('edit_article', kwargs = {'id':'1'} ))
+        response = self.client.get(reverse('edit_article', kwargs={'id': '1'}))
         self.assertEqual(response.status_code, 302)
-
-        response = self.client.post(reverse('edit_article', kwargs = {'id':'1'} ))
+        response = self.client.post(reverse('edit_article',
+                                            kwargs={'id': '1'}))
         self.assertEqual(response.status_code, 302)
-
         self.client.login(username="teste12345", password="supersecret123")
-        response = self.client.get(reverse('edit_article', kwargs = {'id':'1'} ), user=user2)
+        response = self.client.get(reverse('edit_article', kwargs={'id': '1'}),
+                                   user=user2)
         self.assertEqual(response.status_code, 200)
-
-        response = self.client.post(reverse('edit_article', kwargs = {'id':'1'} ))
+        response = self.client.post(reverse('edit_article',
+                                            kwargs={'id': '1'}))
         self.assertEqual(response.status_code, 200)
