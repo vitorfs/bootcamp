74b79c4e29895b706ca89fc9ba86209992c59cc1
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-05-09 22:02:39 -0500

    Adding and small test to the activities app to validate activity registering

--- bootcamp/activities/tests.py
+++ bootcamp/activities/tests.py
@@ -1,1 +1,24 @@
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from bootcamp.activities.models import Activity
+
+
+class TestModels(TestCase):
+    """TestCase class to test the models functionality
+    """
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            email='test@gmail.com',
+            password='top_secret'
+        )
+
+    def test_register_fav_activity(self):
+        activity = Activity.objects.create(
+            user=self.user,
+            activity_type='F'
+        )
+        self.assertTrue(isinstance(activity, Activity))
+        self.assertEqual(str(activity), 'F')
+        self.assertNotEqual(str(activity), 'f')
