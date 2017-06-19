e907a4359de7c5ac3179d80681014a7a07a1f7a2
Cem Ersoz <cersozz@gmail.com>
2017-06-19 10:04:22 -0700

    Update views.py

--- bootcamp/feeds/views.py
+++ bootcamp/feeds/views.py
@@ -1,5 +1,7 @@
 import json
-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import (HttpResponse, HttpResponseBadRequest,
