bcaef1f9ba6617691d6a22c16b39dc6f04c58122
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-07-01 21:58:59 -0500

    Defining a second test for the views

--- bootcamp/articles/tests/test_views.py
+++ bootcamp/articles/tests/test_views.py
@@ -30,3 +30,17 @@ class TestViews(TestCase):
         response_no_art = self.client.get(reverse(
             'article', kwargs={'slug': 'no-slug'}))
         self.assertEqual(response_no_art.status_code, 404)
+
+    def test_individual_article(self):
+        title = 'A really nice to-be title'
+        content = '''This is a really good content, just if somebody published
+        it, that would be awesome, but no, nobody wants to publish it, because
+        they know this is just a test, and you know than nobody wants to
+        publish a test, just a test; everybody always wants the real deal.'''
+        response = self.client.post(reverse('write'), {'title': title,
+                                                       'content': content,
+                                                       'status': 'P'})
+        response_arts = self.client.get(reverse('articles'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response_arts.context['article'].slug,
+                         'a-really-nice-to-be-title')
