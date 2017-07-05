01cd2f4906dea07e8f90057df056ec49b86a9e1a
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-07-05 13:24:18 -0500

    Defining the API endpoint, using the previous content.

--- bootcamp/core/views.py
+++ bootcamp/core/views.py
@@ -41,25 +41,12 @@ def network(request):
 @login_required
 def profile(request, username):
     page_user = get_object_or_404(User, username=username)
-    feeds_count = Feed.objects.filter(user=page_user).count()
-    article_count = Article.objects.filter(create_user=page_user).count()
-    article_comment_count = ArticleComment.objects.filter(
-        user=page_user).count()
-    question_count = Question.objects.filter(user=page_user).count()
-    answer_count = Answer.objects.filter(user=page_user).count()
     all_feeds = Feed.get_feeds().filter(user=page_user)
     paginator = Paginator(all_feeds, FEEDS_NUM_PAGES)
     feeds = paginator.page(1)
     from_feed = -1
     data = {
         'page_user': page_user,
-        'feeds_count': feeds_count,
-        'article_count': article_count,
-        'article_comment_count': article_comment_count,
-        'question_count': question_count,
-        'answer_count': answer_count,
-        'graph_data': [feeds_count, article_count, article_comment_count,
-                       question_count, answer_count],
         'feeds': feeds,
         'from_feed': from_feed,
         'page': 1
@@ -68,6 +55,28 @@ def profile(request, username):
         from_feed = feeds[0].id
 
     return render(request, 'core/profile.html', data)
+
+
+def json_data(request, *args, **kwargs):
+    page_user = User.objects.get(username=request.user.username)
+    feeds_count = Feed.objects.filter(user=page_user).count()
+    article_count = Article.objects.filter(create_user=page_user).count()
+    article_comment_count = ArticleComment.objects.filter(
+        user=page_user).count()
+    question_count = Question.objects.filter(user=page_user).count()
+    answer_count = Answer.objects.filter(user=page_user).count()
+    data = {
+        'page_user': str(page_user),
+        'feeds_count': feeds_count,
+        'article_count': article_count,
+        'article_comment_count': article_comment_count,
+        'question_count': question_count,
+        'answer_count': answer_count,
+        'graph_data': [feeds_count, article_count, article_comment_count,
+                       question_count, answer_count],
+        'labels': ['Feeds', 'Articles', 'Comments', 'Questions', 'Answers'],
+        }
+    return JsonResponse(data)
 
 
 @login_required
