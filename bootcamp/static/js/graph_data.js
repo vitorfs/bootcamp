9c03db57ddb89a857d01a318f7d3efc18ceef064
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-07-05 23:23:12 -0500

    Adding the new line graph, not ready yet, but close to what I am looking for.

--- bootcamp/static/js/graph_data.js
+++ bootcamp/static/js/graph_data.js
@@ -10,7 +10,8 @@ $.ajax({
         // var article_comment_count = request_data.article_comment_count,
         // var question_count = request_data.question_count,
         // var answer_count = request_data.answer_count,
-        setColumnChart();
+        setBarChart();
+        setLineChart();
     },
     error: function(error_data){
         console.log('Error:'),
@@ -18,10 +19,48 @@ $.ajax({
     }
 })
 
-function setColumnChart(){
+function setBarChart(){
     var ctx = document.getElementById("bar_chart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
+        data: {
+            labels: bar_labels,
+            datasets: [{
+                label: 'Activities by type:',
+                data: bar_data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function setLineChart(){
+    var ctx = document.getElementById("line_chart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
         data: {
             labels: bar_labels,
             datasets: [{
