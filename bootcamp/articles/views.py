830a9146db148acb09cbb56437c36f69d67ae897
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-07-26 00:01:38 -0500

    Deleting unused and unnecesary code because of the new implementation.

--- bootcamp/articles/views.py
+++ bootcamp/articles/views.py
@@ -75,12 +75,12 @@ def write(request):
             status = form.cleaned_data.get('status')
             if status in [Article.PUBLISHED, Article.DRAFT]:
                 article.status = form.cleaned_data.get('status')
+
             article.save()
-            tags = form.cleaned_data.get('tags')
-            article.create_tags(tags)
             return redirect('/articles/')
     else:
         form = ArticleForm()
+
     return render(request, 'articles/write.html', {'form': form})
 
 
@@ -93,12 +93,9 @@ def drafts(request):
 
 @login_required
 def edit(request, id):
-    tags = ''
     if id:
         article = get_object_or_404(Article, pk=id)
-        for tag in article.get_tags():
-            tags = '{0} {1}'.format(tags, tag.tag)
-        tags = tags.strip()
+
     else:
         article = Article(create_user=request.user)
 
@@ -110,8 +107,10 @@ def edit(request, id):
         if form.is_valid():
             form.save()
             return redirect('/articles/')
+
     else:
-        form = ArticleForm(instance=article, initial={'tags': tags})
+        form = ArticleForm(instance=article)
+
     return render(request, 'articles/edit.html', {'form': form})
 
 
