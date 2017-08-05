f94f37c5be2b03d51bd48266a95355c85948abfd
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-08-05 07:27:47 -0500

    Adding new tests.

---
+++ bootcamp/core/tests/test_views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
+
+
+class TestViews(TestCase):
+    """
+    Includes tests for all the functionality
+    associated with Views
+    """
+    def setUp(self):
+        self.client = Client()
+        self.other_client = Client()
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            email='test@gmail.com',
+            password='top_secret'
+        )
+        self.other_user = get_user_model().objects.create_user(
+            username='other_test_user',
+            email='other_test@gmail.com',
+            password='top_secret'
+        )
+        self.kwargs = {'content_type': 'application/json',
+                       'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
+        self.client.login(username='test_user', password='top_secret')
+
+    def test_get_home_response(self):
+        response = self.client.get(reverse('feeds'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('<Page 1 of 1>', str(response.context['feeds']))
+
+    def test_get_home_response_no_logged(self):
+        response = self.other_client.get(reverse('feeds'))
+        self.assertRedirects(response, '/?next=/feeds/', status_code=302)
