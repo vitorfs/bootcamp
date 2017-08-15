c0d9403364f141d4acc43a02cd6ec1d66de19d86
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-08-15 06:20:04 -0500

    Improving the coverage and testing some more.

--- bootcamp/questions/tests/tests_models.py
+++ bootcamp/questions/tests/tests_models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from bootcamp.activities.models import Activity
-from bootcamp.questions.models import Question
+from bootcamp.questions.models import Question, Answer
 
 
 class QuestionVoteTest(TestCase):
@@ -17,30 +17,47 @@ class QuestionVoteTest(TestCase):
             email='other_test@gmail.com',
             password='top_secret'
         )
-        self.question = Question.objects.create(
+        self.question_one = Question.objects.create(
             user=self.user, title='This is a sample question',
             description='This is a sample question description',
             tags='test1,test2')
+        self.question_two = Question.objects.create(
+            user=self.user,
+            title='A Short Title',
+            description='A reaaaaally loooong content',
+            favorites=0,
+            has_accepted_answer=True
+        )
+        self.answer = Answer.objects.create(
+            user=self.user,
+            question=self.question_two,
+            description='A reaaaaally loooong content',
+            votes=0,
+            is_accepted=True
+        )
 
     def test_can_up_vote(self):
         activity = Activity.objects.create(user=self.user, activity_type='U',
-                                           question=self.question.id)
+                                           question=self.question_one.id)
         activity.save()
         activity = Activity.objects.create(user=self.user, activity_type='U',
-                                           question=self.question.id)
+                                           question=self.question_one.id)
         activity.save()
-        self.assertEqual(self.question.calculate_votes(), 2)
+        self.assertEqual(self.question_one.calculate_votes(), 2)
 
     def test_can_down_vote(self):
-        votes = self.question.calculate_votes()
+        votes = self.question_one.calculate_votes()
         activity = Activity.objects.create(user=self.user, activity_type='D',
-                                           question=self.question.id)
+                                           question=self.question_one.id)
         activity.save()
-        self.assertEqual(self.question.calculate_votes(), votes - 1)
+        self.assertEqual(self.question_one.calculate_votes(), votes - 1)
 
     def test_str_return_value(self):
-        self.assertTrue(isinstance(self.question, Question))
-        self.assertEqual(str(self.question), 'This is a sample question')
+        self.assertTrue(isinstance(self.question_one, Question))
+        self.assertEqual(str(self.question_one), 'This is a sample question')
 
-    def test_non_aswered_question(self):
-        self.assertEqual(self.question, self.question.get_unanswered()[0])
+    def test_non_answered_question(self):
+        self.assertEqual(self.question_one, Question.get_unanswered()[0])
+
+    def test_answered_question(self):
+        self.assertEqual(self.question_two, Question.get_answered()[0])
