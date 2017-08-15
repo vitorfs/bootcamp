2403a62faa2d810d44a129c26060410d304e376a
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-08-15 06:07:19 -0500

    Improving a little bit the tests.

--- bootcamp/questions/tests/tests_models.py
+++ bootcamp/questions/tests/tests_models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from bootcamp.activities.models import Activity
@@ -7,30 +7,33 @@ from bootcamp.questions.models import Question
 
 class QuestionVoteTest(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username='testuser', password='testpassword',
-                                        email='testuser@example.com')
-        question = Question.objects.create(user=user,
-                                           title='This is a sample question',
-                                           description='This is a sample question description',
-                                           tags='test1,test2')
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            email='test@gmail.com',
+            password='top_secret'
+        )
+        self.other_user = get_user_model().objects.create_user(
+            username='other_test_user',
+            email='other_test@gmail.com',
+            password='top_secret'
+        )
+        self.question = Question.objects.create(
+            user=self.user, title='This is a sample question',
+            description='This is a sample question description',
+            tags='test1,test2')
 
     def test_can_up_vote(self):
-        question = Question.objects.get(title__exact='This is a sample question')
-        activity = Activity.objects.create(user=User.objects.get(username='testuser'),
-                                           activity_type='U',
-                                           question=question.id)
+        activity = Activity.objects.create(user=self.user, activity_type='U',
+                                           question=self.question.id)
         activity.save()
-        activity = Activity.objects.create(user=User.objects.get(username='testuser'),
-                                           activity_type='U',
-                                           question=question.id)
+        activity = Activity.objects.create(user=self.user, activity_type='U',
+                                           question=self.question.id)
         activity.save()
-        self.assertEqual(question.calculate_votes(), 2)
+        self.assertEqual(self.question.calculate_votes(), 2)
 
     def test_can_down_vote(self):
-        question = Question.objects.get(title__exact='This is a sample question')
-        votes = question.calculate_votes()
-        activity = Activity.objects.create(user=User.objects.get(username='testuser'),
-                                           activity_type='D',
-                                           question=question.id)
+        votes = self.question.calculate_votes()
+        activity = Activity.objects.create(user=self.user, activity_type='D',
+                                           question=self.question.id)
         activity.save()
-        self.assertEqual(question.calculate_votes(), votes - 1)
+        self.assertEqual(self.question.calculate_votes(), votes - 1)
