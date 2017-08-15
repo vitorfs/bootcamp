6b40387d3f71d01bfdaa863a166e4c9a48e11619
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-08-14 22:02:00 -0500

    Defining the first fixtures for the tests.

---
+++ bootcamp/feeds/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from bootcamp.articles.models import Article, ArticleComment
+
+
+class TestModels(TestCase):
+    """TestCase class to test the models functionality
+    """
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            email='test@gmail.com',
+            password='top_secret'
+        )
+        self.other_user = get_user_model().objects.create_user(
+            username='other_test_user',
+            email='other_test@gmail.com',
+            password='top_secret'
+        )
