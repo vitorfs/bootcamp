dfea46c1a6a3adba94956d24ed3748cb88d5e8a0
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-08-15 07:18:29 -0500

    Improving the coverage, providing better test titles and testing some more.

--- bootcamp/questions/tests/tests_models.py
+++ bootcamp/questions/tests/tests_models.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 from bootcamp.activities.models import Activity
 from bootcamp.questions.models import Question, Answer
+from bootcamp.activities.models import Activity
 
 
 class QuestionVoteTest(TestCase):
@@ -40,7 +41,7 @@ class QuestionVoteTest(TestCase):
             is_accepted=True
         )
 
-    def test_can_up_vote(self):
+    def test_can_up_vote_question(self):
         activity = Activity.objects.create(user=self.user, activity_type='U',
                                            question=self.question_one.id)
         activity.save()
@@ -49,33 +50,33 @@ class QuestionVoteTest(TestCase):
         activity.save()
         self.assertEqual(self.question_one.calculate_votes(), 2)
 
-    def test_can_down_vote(self):
+    def test_can_down_vote_question(self):
         votes = self.question_one.calculate_votes()
         activity = Activity.objects.create(user=self.user, activity_type='D',
                                            question=self.question_one.id)
         activity.save()
         self.assertEqual(self.question_one.calculate_votes(), votes - 1)
 
-    def test_str_return_value(self):
+    def test_question_str_return_value(self):
         self.assertTrue(isinstance(self.question_one, Question))
         self.assertEqual(str(self.question_one), 'This is a sample question')
 
-    def test_non_answered_question(self):
+    def test_question_non_answered_question(self):
         self.assertEqual(self.question_one, Question.get_unanswered()[0])
 
-    def test_answered_question(self):
+    def test_question_answered_question(self):
         self.assertEqual(self.question_two, Question.get_answered()[0])
 
-    def test_answers_returns(self):
+    def test_question_answers_returns(self):
         self.assertEqual(self.answer, self.question_two.get_answers()[0])
 
-    def test_answer_count(self):
+    def test_question_answer_count(self):
         self.assertEqual(self.question_two.get_answers_count(), 1)
 
-    def test_accepted_answer(self):
+    def test_question_accepted_answer(self):
         self.assertEqual(self.question_two.get_accepted_answer(), self.answer)
 
-    def test_markdown_return(self):
+    def test_question_markdown_return(self):
         self.assertEqual(self.question_one.get_description_as_markdown(),
                          '<p>This is a sample question description</p>')
         self.assertEqual(self.question_two.get_description_as_markdown(),
@@ -85,10 +86,49 @@ class QuestionVoteTest(TestCase):
             know than nobody wants to publish a test, just a test;
             everybody always wants the real deal.</p>''')
 
-    def test_return_summary(self):
+    def test_question_return_summary(self):
         self.assertEqual(len(self.question_two.get_description_preview()), 258)
         self.assertEqual(self.question_two.get_description_preview(),
                          '''This is a really good content, just if somebody
             published it, that would be awesome, but no, nobody wants to
             publish it, because they know this is just a test, and you
             know than nobody wants to publish a test, just a te...''')
+        self.assertEqual(self.question_one.get_description_preview(),
+                         'This is a sample question description')
+
+    def test_question_markdown_description_preview(self):
+        self.assertTrue(
+            self.question_two.get_description_preview_as_markdown(),
+            '''<p>This is a really good content, just if somebody
+            published it, that would be awesome, but no, nobody wants to
+            publish it, because they know this is just a test, and you
+            know than nobody wants to publish a test, just a te...</p>''')
+
+    def test_favorite_question(self):
+        activity = Activity.objects.create(
+            user=self.user,
+            activity_type='F',
+            question=self.question_one.id
+        )
+        self.assertTrue(isinstance(activity, Activity))
+        self.assertEqual(self.question_one.calculate_favorites(), 1)
+
+    def test_question_favoriters(self):
+        activity = Activity.objects.create(
+            user=self.user,
+            activity_type='F',
+            question=self.question_one.id
+        )
+        self.assertTrue(isinstance(activity, Activity))
+        self.assertEqual(self.user, self.question_one.get_favoriters()[0].user)
+
+    def test_question_voters_retun_values(self):
+        activity = Activity.objects.create(user=self.user, activity_type='U',
+                                           question=self.question_one.id)
+        activity = Activity.objects.create(user=self.other_user,
+                                           activity_type='D',
+                                           question=self.question_one.id)
+        self.assertTrue(isinstance(activity, Activity))
+        self.assertEqual(self.question_one.get_up_voters()[0].user, self.user)
+        self.assertEqual(
+            self.question_one.get_down_voters()[0].user, self.other_user)
