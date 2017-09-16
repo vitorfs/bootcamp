bd182ec6fdcde5914f680d454877307404fd7e83
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-09-16 12:27:20 -0500

    Deleting the full resources on the new message view.

--- bootcamp/messenger/views.py
+++ bootcamp/messenger/views.py
@@ -56,38 +56,6 @@ def messages(request, username):
 
 
 @login_required
-def new(request):
-    if request.method == 'POST':
-        from_user = request.user
-        to_user_username = request.POST.get('to')
-        try:
-            to_user = User.objects.get(username=to_user_username)
-
-        except Exception:
-            try:
-                to_user_username = to_user_username[
-                    to_user_username.rfind('(')+1:len(to_user_username)-1]
-                to_user = User.objects.get(username=to_user_username)
-
-            except Exception:
-                return redirect('/messages/new/')
-
-        message = request.POST.get('message')
-        if len(message.strip()) == 0:
-            return redirect('/messages/new/')
-
-        if from_user != to_user:
-            Message.send_message(from_user, to_user, message)
-
-        return redirect('/messages/{0}/'.format(to_user_username))
-
-    else:
-        conversations = Message.get_conversations(user=request.user)
-        return render(request, 'messenger/new.html',
-                      {'conversations': conversations})
-
-
-@login_required
 @ajax_required
 def delete(request):
     return HttpResponse()
