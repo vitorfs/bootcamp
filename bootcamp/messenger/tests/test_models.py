5cf520710479a03eb4e6b1fd81e01b23ac1d37dd
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2017-09-16 20:58:50 -0500

    Additional tests.

--- bootcamp/messenger/tests/test_models.py
+++ bootcamp/messenger/tests/test_models.py
@@ -39,3 +39,19 @@ class TestModels(TestCase):
 
     def test_object_instance(self):
         self.assertTrue(isinstance(self.message_one, Message))
+
+    def test_return_values(self):
+        self.assertEqual(self.message_one.message, "A not that long message")
+        self.assertEqual(self.message_three.message, "A shorter message")
+        self.assertEqual(Message.get_conversations(self.user)[0]['last'],
+                         self.message_two.date)
+        self.assertEqual(Message.get_conversations(self.other_user)[0]['last'],
+                         self.message_three.date)
+
+    def test_sending_new_message(self):
+        new_message = Message.send_message(
+            self.other_user, self.user, "A short message")
+        self.assertTrue(isinstance(new_message, Message))
+        self.assertEqual(Message.get_conversations(self.other_user)[0]['last'],
+                         new_message.date)
+        self.assertEqual(new_message.message, "A short message")
