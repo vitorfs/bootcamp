5ca6ec3412b3ed1c93a6acf4cc376a79369c2ff2
Sebastian Reyes <sebaslander@gmail.com>
2017-12-04 21:54:34 -0500

    Fixing list of error from Flake8 as: Error E501 line too long (92 > 80 characters), Error F401 'HttpResponseBadRequest' imported but unused, Error F401 'reverse' imported but unused, Error E501 line too long (97 > 80 characters), Error E501 line too long (94 > 80 characters), Error F401 'HttpResponseBadRequest' imported but unused, Error F401 'HttpResponseBadRequest' imported but unused, Error F401 'HttpResponseBadRequest' imported but unused, Error F841 local variable 'e' is assigned to but never used.

--- bootcamp/messenger/tests/test_views.py
+++ bootcamp/messenger/tests/test_views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.http import HttpResponseBadRequest
 from django.core.urlresolvers import reverse
 from django.test import Client, TestCase
 
