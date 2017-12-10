7d8f273ca01462adf4b906587bdd3ebbf4be1426
Sebastian Reyes <sebaslander@gmail.com>
2017-12-10 15:54:34 -0500

    Improving the factories and adding new tests.

--- bootcamp/feeds/tests/test_views.py
+++ bootcamp/feeds/tests/test_views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import get_user_model
-from django.core.urlresolvers import reverse
 from django.test import Client, TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
 
-from bootcamp.feeds.models import Feed
+from bootcamp.factories import UserFactory, FeedsFactory
 
 
 class TestViews(TestCase):
@@ -11,27 +11,30 @@ class TestViews(TestCase):
     associated with Views
     """
     def setUp(self):
-        self.client = Client()
-        self.other_client = Client()
         self.user = get_user_model().objects.create_user(
             username='test_user',
             email='test@gmail.com',
             password='top_secret'
         )
-        self.other_user = get_user_model().objects.create_user(
-            username='other_test_user',
-            email='other_test@gmail.com',
-            password='top_secret'
-        )
+        self.other_user = UserFactory()
+        self.client = Client()
         self.client.login(username='test_user', password='top_secret')
-        self.other_client.login(
-            username='other_test_user', password='top_secret')
-        self.feed = Feed.objects.create(
-            user=self.user,
-            post='A not so long text',
-            likes=0,
-            comments=0)
+        # self.client.login(
+        #     username=self.other_user.username,
+        #     password=self.other_user.password
+        # )
+        self.feed = FeedsFactory(
+            user=self.user
+        )
 
-    def test_feed_view(self):
+    def test_feeds_view(self):
+        request = self.client.get(reverse('feeds'))
+        assert request.status_code == 200
+
+    def test_feed_alone(self):
         request = self.client.get(reverse('feed', args=[self.feed.id]))
-        self.assertEqual(request.status_code, 200)
+        assert request.status_code == 200
+
+    def test_load_view(self):
+        request = self.client.get(reverse('load'), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        assert request.status_code == 200
