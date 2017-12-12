08ad15491f8b966bc5a9a817e1b938f80e40c733
Sebastian Reyes <sebaslander@gmail.com>
2017-12-12 16:16:06 -0500

    Defining the basic functionality of the corresponding consumers, still lacking a proper structure, and a with a big need of learning.

---
+++ bootcamp/messenger/consumers.py
@@ -0,0 +1,38 @@
+import json
+from channels import Group
+from channels.auth import channel_session_user, channel_session_user_from_http
+
+
+@channel_session_user_from_http
+def ws_connect(message):
+    # Adds to inbox group.
+    Group('inbox-{}'.format(
+        message.user.username)).add(message.reply_channel)
+    # Group('users').send({
+    #     'text': json.dumps({
+    #         'username': message.user.username,
+    #         'is_logged_in': True
+    #     })
+    # })
+
+
+@channel_session_user
+def ws_disconnect(message):
+    # Group('users').send({
+    #     'text': json.dumps({
+    #         'username': message.user.username,
+    #         'is_logged_in': False
+    #     })
+    # })
+    # Group('users').discard(message.reply_channel)
+    Group('inbox-{}'.format(
+        message.user.username)).discard(message.reply_channel)
+
+
+def ws_receive(message):
+    Group('inbox-{}'.format(message.user.username)).send({
+        "text": json.dumps({
+            "id": instance.id,
+            "content": instance.content
+        })
+    })
