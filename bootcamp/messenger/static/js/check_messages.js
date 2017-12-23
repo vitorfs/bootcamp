f58dddc8d1fdc6a05482559afc4761f01c2a86f8
Sebastian Reyes <sebaslander@gmail.com>
2017-12-23 06:26:41 -0500

    Adding some style to the JS files before the storm.

--- bootcamp/messenger/static/js/check_messages.js
+++ bootcamp/messenger/static/js/check_messages.js
@@ -1,17 +1,17 @@
 $(function () {
-  function check_messages() {
-    $.ajax({
-      url: '/messages/check/',
-      cache: false,
-      success: function (data) {
-        $("#unread-count").text(data);
-      },
-      complete: function () {
-        window.setTimeout(check_messages, 60000);
-      }
-    });
-  };
-  check_messages();
+    function check_messages() {
+        $.ajax({
+            url: '/messages/check/',
+            cache: false,
+            success: function (data) {
+                $("#unread-count").text(data);
+            },
+            complete: function () {
+                window.setTimeout(check_messages, 60000);
+            }
+        });
+    }
+    check_messages();
 });
 
 $(function () {
--- bootcamp/messenger/static/js/messages.js
+++ bootcamp/messenger/static/js/messages.js
@@ -1,17 +1,17 @@
 $(function () {
-  $("#send").submit(function () {
-    $.ajax({
-      url: '/messages/send/',
-      data: $("#send").serialize(),
-      cache: false,
-      type: 'post',
-      success: function (data) {
-        $(".send-message").before(data);
-        $("input[name='message']").val('');
-        $("input[name='message']").focus();
-        $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
-      }
-    });
+    $("#send").submit(function () {
+        $.ajax({
+            url: '/messages/send/',
+            data: $("#send").serialize(),
+            cache: false,
+            type: 'post',
+            success: function (data) {
+                $(".send-message").before(data);
+                $("input[name='message']").val('');
+                $("input[name='message']").focus();
+                $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
+            }
+        });
     return false;
-  });
+    });
 });
