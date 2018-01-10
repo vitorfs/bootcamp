3bf580884f9fe9e52d0d1271a572525a8b06e6a4
Sebastian Reyes <sebaslander@gmail.com>
2018-01-10 15:48:19 -0500

    Improving the consumers for better and wider funcitonality.

--- bootcamp/activities/consumers.py
+++ bootcamp/activities/consumers.py
@@ -1,9 +1,25 @@
+import json
 from channels import Group
+from channels.auth import channel_session_user, channel_session_user_from_http
 
 
+@channel_session_user_from_http
 def ws_connect(message):
     Group('notifications').add(message.reply_channel)
+    Group('notifications').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': True
+        })
+    })
 
 
+@channel_session_user
 def ws_disconnect(message):
-    Group('notifications').discard(message.reply_channel)   
+    Group('notifications').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': False
+        })
+    })
+    Group('notifications').discard(message.reply_channel)
