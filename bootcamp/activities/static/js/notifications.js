6e683407c9ffa8f898ad18edf7ac32a335cdb8f7
Sebastian Reyes <sebaslander@gmail.com>
2018-01-10 15:32:03 -0500

    Small addition to the JS file to conect to WebSockets.

--- bootcamp/activities/static/js/notifications.js
+++ bootcamp/activities/static/js/notifications.js
@@ -1,42 +1,52 @@
 $(function () {
-  $('#notifications').popover({html: true, content: 'Loading...', trigger: 'manual'});
+    $('#notifications').popover({html: true, content: 'Loading...', trigger: 'manual'});
 
-  $("#notifications").click(function () {
-    if ($(".popover").is(":visible")) {
-      $("#notifications").popover('hide');
-    }
-    else {
-      $("#notifications").popover('show');
-      $.ajax({
-        url: '/notifications/last/',
-        beforeSend: function () {
-          $(".popover-content").html("<div style='text-align:center'><img src='/static/img/loading.gif'></div>");
-          $("#notifications").removeClass("new-notifications");
-        },
-        success: function (data) {
-          $(".popover-content").html(data);
-        }
-      });
-    }
-    return false;
-  });
-
-  function check_notifications() {
-    $.ajax({
-      url: '/notifications/check/',
-      cache: false,
-      success: function (data) {
-        if (data != "0") {
-          $("#notifications").addClass("new-notifications");
+    $("#notifications").click(function () {
+        if ($(".popover").is(":visible")) {
+            $("#notifications").popover('hide');
         }
         else {
-          $("#notifications").removeClass("new-notifications");
+            $("#notifications").popover('show');
+            $.ajax({
+                url: '/notifications/last/',
+                beforeSend: function () {
+                    $(".popover-content").html("<div style='text-align:center'><img src='/static/img/loading.gif'></div>");
+                    $("#notifications").removeClass("new-notifications");
+                },
+                success: function (data) {
+                    $(".popover-content").html(data);
+                }
+            });
         }
-      },
-      complete: function () {
-        window.setTimeout(check_notifications, 30000);
-      }
+        return false;
     });
-  };
-  check_notifications();
+
+    function check_notifications() {
+        var socket = new WebSocket('ws://' + window.location.host + '/notifications/');
+        socket.onopen = function open() {
+            console.log('WebSockets connection created to notification channel.');
+        };
+        
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.onopen();
+        };
+    };
+    /* function check_notifications() {
+        $.ajax({
+            url: '/notifications/check/',
+            cache: false,
+            success: function (data) {
+                if (data != "0") {
+                    $("#notifications").addClass("new-notifications");
+                }
+                else {
+                    $("#notifications").removeClass("new-notifications");
+                }
+            },
+            complete: function () {
+                window.setTimeout(check_notifications, 30000);
+            }
+        });
+    }; */
+    check_notifications();
 });
