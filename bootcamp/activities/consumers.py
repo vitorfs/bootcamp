e78ee302aefd1832b797cbba78004a0f1a8643f5
Sebastian Reyes <sebaslander@gmail.com>
2018-01-11 14:33:20 -0500

    Adding the necessary elements to allow the websocket comunication for the notifications Group.

--- bootcamp/activities/consumers.py
+++ bootcamp/activities/consumers.py
@@ -5,21 +5,21 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 
 @channel_session_user_from_http
 def ws_connect(message):
+    message.reply_channel.send({"accept": True})
     Group('notifications').add(message.reply_channel)
-    Group('notifications').send({
-        'text': json.dumps({
-            'username': message.user.username,
-            'is_logged_in': True
-        })
-    })
 
 
 @channel_session_user
 def ws_disconnect(message):
-    Group('notifications').send({
-        'text': json.dumps({
-            'username': message.user.username,
-            'is_logged_in': False
-        })
-    })
     Group('notifications').discard(message.reply_channel)
+
+
+@channel_session_user
+def ws_receive(message):
+    Group('notifications').send({
+            'text': json.dumps({
+                'username': message.user.username,
+                'activity_type': message.activity_type,
+                'activity': message.activity
+            })
+        })
