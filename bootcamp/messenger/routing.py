c439c599c9abdd4ac7178f4ad8c3728945370818
Sebastian Reyes <sebaslander@gmail.com>
2018-01-14 07:43:05 -0500

    Cleaning the routing definition.

--- bootcamp/messenger/routing.py
+++ bootcamp/messenger/routing.py
@@ -4,12 +4,7 @@ from bootcamp.messenger.consumers import ws_connect, ws_receive, ws_disconnect
 
 
 websocket_routing = [
-    # Called when WebSockets connect
     route("websocket.connect", ws_connect),
-
-    # Called when WebSockets get sent a data frame
     route("websocket.receive", ws_receive),
-
-    # Called when WebSockets disconnect
     route("websocket.disconnect", ws_disconnect),
 ]
