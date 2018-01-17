c7c9439dfe377081e96f58a60639a821893416d3
Sebastian Reyes <sebaslander@gmail.com>
2018-01-17 10:16:55 -0500

    Changing the consumers.

--- bootcamp/messenger/consumers.py
+++ bootcamp/messenger/consumers.py
@@ -6,21 +6,17 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 @channel_session_user_from_http
 def ws_connect(message):
     message.reply_channel.send({"accept": True})
-    Group('inbox-{}'.format(
-        message.user.username)).add(message.reply_channel)
+    Group('inbox').add(message.reply_channel)
 
 
 @channel_session_user
 def ws_disconnect(message):
-    Group('inbox-{}'.format(
-        message.user.username)).discard(message.reply_channel)
+    Group('inbox').discard(message.reply_channel)
 
 
 @channel_session_user
 def ws_receive(message):
-    print(dir(message))
-    print("this line")
-    Group('inbox-{}'.format(message.receiver)).send({
+    Group('inbox').send({
         "text": json.dumps({
             "content": message.content
         })
