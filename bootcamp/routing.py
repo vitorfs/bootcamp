726a51396140b6c0ecd4c13d4fccd77875a5e9ac
Sebastian Reyes <sebaslander@gmail.com>
2018-01-17 10:40:32 -0500

    Typo.

--- bootcamp/routing.py
+++ bootcamp/routing.py
@@ -3,5 +3,5 @@ from channels import include
 channel_routing = [
     # Include subrouting from an app with predefined path matching.
     include("bootcamp.activities.routing.websocket_routing",
-            path=r"^/notifications/"),
+            path=r"^/notifications/$"),
 ]
