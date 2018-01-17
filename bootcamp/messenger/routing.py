39e723c81318c59baedd7609449ce4967fcd2c51
Sebastian Reyes <sebaslander@gmail.com>
2018-01-17 10:16:09 -0500

    Definig a new path for the rounting for better separation.

--- bootcamp/messenger/routing.py
+++ bootcamp/messenger/routing.py
@@ -4,7 +4,7 @@ from bootcamp.messenger.consumers import ws_connect, ws_receive, ws_disconnect
 
 
 websocket_routing = [
-    route("websocket.connect", ws_connect),
+    route("websocket.connect", ws_connect, path=r'^/(?P<username>[^/]+)/$'),
     route("websocket.receive", ws_receive),
     route("websocket.disconnect", ws_disconnect),
 ]
