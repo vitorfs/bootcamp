9d1a2e8ea945b0522c04142492c4ceea4130277c
Sebastian Reyes <sebaslander@gmail.com>
2018-01-17 10:15:55 -0500

    Definig a new path for the rounting for better separation.

--- bootcamp/routing.py
+++ bootcamp/routing.py
@@ -3,7 +3,7 @@ from channels import include
 channel_routing = [
     # Include subrouting from an app with predefined path matching.
     include("bootcamp.activities.routing.websocket_routing",
-            path=r"^/notifications/"),
+            path=r"^/notifications/$"),
     include("bootcamp.messenger.routing.websocket_routing",
-            path=r"^/messenger/inbox"),
+            path=r"^/inbox"),
 ]
