b366571d7700a201bc627b48ffbfc9f6f987b295
Sebastian Reyes <sebaslander@gmail.com>
2018-01-19 17:13:43 -0500

    Adding the new routing.

--- bootcamp/routing.py
+++ bootcamp/routing.py
@@ -4,4 +4,5 @@ channel_routing = [
     # Include subrouting from an app with predefined path matching.
     include("bootcamp.activities.routing.websocket_routing",
             path=r"^/notifications/$"),
+    include("bootcamp.feeds.routing.websocket_routing", path=r"^/feeds/$")
 ]
