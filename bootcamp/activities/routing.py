092f57baedf41ea51abf34f38da3c77584c720ca
Sebastian Reyes <sebaslander@gmail.com>
2018-01-19 15:14:03 -0500

    I was mistaken, I deleted this code because I haven't realized properly than it was not required at this level of functionality.

--- bootcamp/activities/routing.py
+++ bootcamp/activities/routing.py
@@ -1,9 +1,8 @@
 from channels.routing import route
-from bootcamp.activities.consumers import ws_connect, ws_disconnect, ws_receive
+from bootcamp.activities.consumers import ws_connect, ws_disconnect
 
 
 websocket_routing = [
     route('websocket.connect', ws_connect),
-    route('websocket.receive', ws_receive),
     route('websocket.disconnect', ws_disconnect),
 ]
