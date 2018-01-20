b9b046aa5607c6721e4b6c4f6de5fab8e0db5a85
Sebastian Reyes <sebaslander@gmail.com>
2018-01-20 10:41:31 -0500

    Deleting not functional code at this point.

--- bootcamp/messenger/routing.py
+++ bootcamp/messenger/routing.py
@@ -1,10 +1,9 @@
 from channels import route
 
-from bootcamp.messenger.consumers import ws_connect, ws_receive, ws_disconnect
+from bootcamp.messenger.consumers import ws_connect, ws_disconnect
 
 
 websocket_routing = [
-    route("websocket.connect", ws_connect, path=r'^/(?P<username>[^/]+)/$'),
-    route("websocket.receive", ws_receive),
+    route("websocket.connect", ws_connect),
     route("websocket.disconnect", ws_disconnect),
 ]
