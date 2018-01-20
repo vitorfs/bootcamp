46d20ece9e0c1f3043ba601f12a7c2eccb915856
Sebastian Reyes <sebaslander@gmail.com>
2018-01-20 10:39:38 -0500

    Deleting unnecesary code.

--- bootcamp/messenger/static/js/check_messages.js
+++ bootcamp/messenger/static/js/check_messages.js
@@ -27,19 +27,4 @@ $(function () {
             });
             */
     });
-    /*
-    function check_messages() {
-        $.ajax({
-            url: '/messages/check/',
-            cache: false,
-            success: function (data) {
-                $("#unread-count").text(data);
-            },
-            complete: function () {
-                window.setTimeout(check_messages, 60000);
-            }
-        });
-    }
-    check_messages();
-    */
 });
