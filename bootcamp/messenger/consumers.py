1537d21cfc48b077ad90c5a5638aca040dc084ef
Sebastian Reyes <sebaslander@gmail.com>
2018-01-26 19:32:44 -0500

    Let's manage the notifications in a consolidated way.

--- bootcamp/messenger/consumers.py
+++ bootcamp/messenger/consumers.py
@@ -1,4 +1,5 @@
 import json
+
 from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
 
@@ -6,18 +7,16 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 @channel_session_user_from_http
 def ws_connect(message):
     message.reply_channel.send({"accept": True})
-    Group('inbox').add(message.reply_channel)
+    Group('{}'.format(message.user)).add(message.reply_channel)
 
 
 @channel_session_user
 def ws_disconnect(message):
-    Group('inbox').discard(message.reply_channel)
+    Group('{}'.format(message.user)).discard(message.reply_channel)
 
 
-@channel_session_user
 def ws_receive(message):
-    Group('inbox').send({
-        "text": json.dumps({
-            "content": message.content
-        })
-    })
+    old_message = json.loads(message.content['text'])
+    new_message = {}
+    new_message['text'] = message.content['text']
+    Group(old_message['receiver']).send(new_message)
