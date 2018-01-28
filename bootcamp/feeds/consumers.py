fa39c181040a68cc4620497dbfd36eabe669d983
Sebastian Reyes <sebaslander@gmail.com>
2018-01-28 09:06:53 -0500

    Small changes to the feed app to see if there is an issue than is making it to fail the core functionality.

--- bootcamp/feeds/consumers.py
+++ bootcamp/feeds/consumers.py
@@ -4,7 +4,7 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 
 @channel_session_user_from_http
 def ws_connect(message):
-    # message.reply_channel.send({"accept": True})
+    message.reply_channel.send({"accept": True})
     Group('feeds').add(message.reply_channel)
 
 
