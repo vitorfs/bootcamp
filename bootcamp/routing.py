c5efed643602e36573e6c743491e2eff15d986c3
Sebastian Reyes <sebaslander@gmail.com>
2018-02-02 16:35:44 -0500

    Small changes to fix the issue with the feeds websockets connection flow, which was blocking the notification process.

--- bootcamp/routing.py
+++ bootcamp/routing.py
@@ -4,7 +4,7 @@ channel_routing = [
     # Include subrouting from an app with predefined path matching.
     include("bootcamp.activities.routing.websocket_routing",
             path=r"^/notifications/$"),
+    include("bootcamp.feeds.routing.websocket_routing", path=r"^/feeds/$"),
     include("bootcamp.messenger.routing.websocket_routing",
-            path=r"^/"),
-    include("bootcamp.feeds.routing.websocket_routing", path=r"^/feeds/$")
+            path=r"^/")
 ]
