97e48ff2ad2ebf2d249f47688909a8d7cd7852de
Sebastian Reyes <sebaslander@gmail.com>
2018-02-02 16:35:44 -0500

    Small changes to fix the issue with the feeds websockets connection flow, which was blocking the notification process.

--- bootcamp/feeds/consumers.py
+++ bootcamp/feeds/consumers.py
@@ -4,22 +4,10 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 
 @channel_session_user_from_http
 def ws_connect(message):
-    message.reply_channel.send({"accept": True})
     Group('feeds').add(message.reply_channel)
-    print("Been here!")
+    message.reply_channel.send({"accept": True})
 
 
 @channel_session_user
 def ws_disconnect(message):
     Group('feeds').discard(message.reply_channel)
-
-'''
-@channel_session_user
-def ws_receive(message):
-    Group('feeds').send({
-            'text': json.dumps({
-                'activity': message.activity,
-                'count': message.count
-            })
-        })
- '''
