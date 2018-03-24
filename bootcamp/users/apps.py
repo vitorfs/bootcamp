fd7f6d2d5dce1ab570a3c2bdf9c7f0051aa6435c
Sebastian Reyes <sebaslander@gmail.com>
2018-03-23 22:17:08 -0500

    Adding the new user app.

---
+++ bootcamp/users/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'bootcamp.users'
+    verbose_name = "Users"
+
+    def ready(self):
+        """Override this to put in:
+            Users system checks
+            Users signal registration
+        """
+        try:
+            import users.signals  # noqa F401
+        except ImportError:
+            pass
