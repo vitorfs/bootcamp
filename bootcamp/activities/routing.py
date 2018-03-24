db9033fae723d213ae158c6f3e5e12502b2b828d
Sebastian Reyes <sebaslander@gmail.com>
2018-03-23 22:22:23 -0500

    Deleting the previous bootcamp apps to develop some new ones.

--- bootcamp/activities/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import route
-from bootcamp.activities.consumers import ws_connect, ws_disconnect
-
-
-websocket_routing = [
-    route('websocket.connect', ws_connect),
-    route('websocket.disconnect', ws_disconnect),
-]
