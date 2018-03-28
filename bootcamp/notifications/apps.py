2d8002f6deef5e25a367a0506755fb4b9e84de91
Sebastian Reyes <sebaslander@gmail.com>
2018-03-28 10:04:49 -0500

    Registering the notifications app.

---
+++ bootcamp/notifications/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class NotificationsConfig(AppConfig):
+    name = 'bootcamp.notifications'
+    verbose_name = _('Notifications')
+
+    def ready(self):
+        import bootcamp.notifications.signals
