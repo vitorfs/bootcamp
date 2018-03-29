d28903bc993709b92deb1ccf18f5bef96fd22e49
Sebastian Reyes <sebaslander@gmail.com>
2018-03-29 13:42:22 -0500

    Modifications to the model and the proper migrations.

--- bootcamp/notifications/models.py
+++ bootcamp/notifications/models.py
@@ -1,18 +1,13 @@
-import json
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models.query import QuerySet
-from django.utils.six import text_type
 from django.utils.translation import ugettext_lazy as _
 
 from slugify import slugify
-
-from bootcamp.notifications.signals import notify
 
 
 class NotificationQuerySet(models.query.QuerySet):
@@ -58,14 +53,12 @@ class Notification(models.Model):
     Generalized Format::
 
         <actor> <verb> <time>
-        <actor> <verb> <target> <time>
-        <actor> <verb> <action_object> <target> <time>
+        <actor> <verb> <action_object> <time>
 
     Examples::
 
         <Sebastian> <Logged In> <1 minute ago>
         <Sebastian> <commented> <Article> <2 hours ago>
-        <Sebastian> <commented> <comment> on <question> <about 2 hours ago>
     """
     LIKED = 'L'
     COMMENTED = 'C'
@@ -76,49 +69,37 @@ class Notification(models.Model):
     ALSO_COMMENTED = 'K'
     LOGGED_IN = 'I'
     LOGGED_OUT = 'O'
-    UPVOTED = 'U'
+    VOTED = 'V'
     SHARED = 'S'
     NOTIFICATION_TYPES = (
-        (LIKED, _('Liked')),
-        (COMMENTED, _('Commented')),
-        (FAVORITED, _('Favorited')),
-        (ANSWERED, _('Answered')),
-        (ACCEPTED_ANSWER, _('Accepted')),
-        (EDITED_ARTICLE, _('Edited')),
-        (ALSO_COMMENTED, _('Also commented')),
-        (LOGGED_IN, _('Logged In')),
-        (LOGGED_OUT, _('Logged Out')),
-        (UPVOTED, _('Up voted')),
-        (SHARED, _('Shared')),
+        (LIKED, _('liked')),
+        (COMMENTED, _('commented')),
+        (FAVORITED, _('cavorited')),
+        (ANSWERED, _('answered')),
+        (ACCEPTED_ANSWER, _('accepted')),
+        (EDITED_ARTICLE, _('edited')),
+        (ALSO_COMMENTED, _('also commented')),
+        (LOGGED_IN, _('logged In')),
+        (LOGGED_OUT, _('logged Out')),
+        (VOTED, _('voted on')),
+        (SHARED, _('shared')),
         )
     actor = models.ForeignKey(settings.AUTH_USER_MODEL,
                               related_name='notify_actor',
                               on_delete=models.CASCADE)
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                  related_name='notifications',
-                                  on_delete=models.CASCADE)
-    unread = models.BooleanField(default=True, blank=False, db_index=True)
+    recipient = models.CharField(max_length=250)
+    unread = models.BooleanField(default=True, db_index=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     uuid_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
     slug = models.SlugField(max_length=210, null=True, blank=True)
-
     verb = models.CharField(max_length=1, choices=NOTIFICATION_TYPES)
-
     action_object_content_type = models.ForeignKey(ContentType,
         blank=True, null=True, related_name='notify_action_object',
         on_delete=models.CASCADE)
-    action_object_object_id = models.CharField(
-        max_length=255, blank=True, null=True)
+    action_object_object_id = models.PositiveIntegerField(blank=True, null=True)
     action_object = GenericForeignKey(
         'action_object_content_type', 'action_object_object_id')
-
-    target_content_type = models.ForeignKey(ContentType,
-        related_name='notify_target', blank=True, null=True,
-        on_delete=models.CASCADE)
-    target_object_id = models.CharField(max_length=255, blank=True, null=True)
-    target = GenericForeignKey('target_content_type', 'target_object_id')
-
     objects = NotificationQuerySet.as_manager()
 
     class Meta:
@@ -126,18 +107,11 @@ class Notification(models.Model):
         app_label = 'notifications'
 
     def __str__(self):
-        if self.target:
-            if self.action_object:
-                return f'{self.actor} {self.verb} {self.action_object} on {self.target} {self.time_since} ago'
-
-            return f'{self.actor} {self.verb} {self.target} {self.time_since} ago'
-
         if self.action_object:
-            return f'{self.actor} {self.verb} {self.action_object} {self.time_since} ago'
+            return f'{self.actor} {self.get_verb_display()} {self.action_object} {self.time_since()} ago'
 
-        return f'{actor} {verb} {time_since} ago'
+        return f'{self.actor} {self.get_verb_display()} {self.time_since()} ago'
 
-    @property
     def time_since(self, now=None):
         """
         Shortcut for the ``django.utils.timesince.timesince`` function of the
@@ -166,54 +140,40 @@ class Notification(models.Model):
             self.save()
 
 
-def notify_handler(verb, **kwargs):
+def notification_handler(actor, recipient, verb, **kwargs):
     """
     Handler function to create a Notification instance upon action signal call.
+    :requires:
+    :param actor: User instance of that user who makes the action.
+    :param recipient: String or list of strings defining who should be notified.
+    :param verb: Notification attribute with the right choice from the list.
+
+    :optional:
+    :action_object: Model instance on which the verb was executed.
     """
-    # Pull the options out of kwargs
-    kwargs.pop('signal', None)
-    recipient = kwargs.pop('recipient')
-    actor = kwargs.pop('sender')
-    optional_objs = [
-        (kwargs.pop(opt, None), opt)
-        for opt in ('target', 'action_object')
-    ]
+    if recipient == 'global':
+        users = get_user_model().objects.all().exclude(username=actor.username)
+        for user in users:
+            Notification.objects.create(
+                actor=actor,
+                recipient=user,
+                verb=verb,
+                action_object=kwargs.pop('action_object', None)
+            )
 
-    # Check if User or Group
-    if isinstance(recipient, Group):
-        recipients = recipient.user_set.all()
-
-    elif isinstance(recipient, QuerySet) or isinstance(recipient, list):
-        recipients = recipient
+    elif isinstance(recipient, list):
+        for user in recipient:
+            Notification.objects.create(
+                actor=actor,
+                recipient=get_user_model().objects.get(username=user),
+                verb=verb,
+                action_object=kwargs.pop('action_object', None)
+            )
 
     else:
-        recipients = [recipient]
-
-    new_notifications = []
-
-    for recipient in recipients:
-        newnotify = Notification(
+        Notification.objects.create(
+            actor=actor,
             recipient=recipient,
-            actor_content_type=ContentType.objects.get_for_model(actor),
-            actor_object_id=actor.pk,
-            verb=text_type(verb)
+            verb=verb,
+            action_object=kwargs.pop('action_object', None)
         )
-
-        # Set optional objects
-        for obj, opt in optional_objs:
-            if obj is not None:
-                setattr(newnotify, '%s_object_id' % opt, obj.pk)
-                setattr(newnotify, '%s_content_type' % opt,
-                        ContentType.objects.get_for_model(obj))
-
-        if len(kwargs) and EXTRA_DATA:
-            newnotify.data = kwargs
-
-        newnotify.save()
-        new_notifications.append(newnotify)
-
-    return new_notifications
-
-
-# connect the signal
-notify.connect(notify_handler)
