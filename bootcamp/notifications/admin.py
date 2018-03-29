2b6d349892634b58607826e11e75d45cd6a40c81
Sebastian Reyes <sebaslander@gmail.com>
2018-03-29 13:41:36 -0500

    Fixing incorrect data.

--- bootcamp/notifications/admin.py
+++ bootcamp/notifications/admin.py
@@ -4,6 +4,5 @@ from bootcamp.notifications.models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    raw_id_fields = ('recipient', )
-    list_display = ('recipient', 'actor', 'target', 'verb', 'unread',)
-    list_filter = ('unread', )
+    list_display = ('recipient', 'actor', 'verb', 'unread', )
+    list_filter = ('recipient', 'unread', )
