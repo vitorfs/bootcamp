bab4ce1c92c4bab0f84a28215e8a629525733d08
Sebastian Reyes <sebaslander@gmail.com>
2018-03-29 13:41:00 -0500

    Deleting the customized signal because is not required.

--- bootcamp/notifications/apps.py
+++ bootcamp/notifications/apps.py
@@ -5,6 +5,3 @@ from django.utils.translation import ugettext_lazy as _
 class NotificationsConfig(AppConfig):
     name = 'bootcamp.notifications'
     verbose_name = _('Notifications')
-
-    def ready(self):
-        import bootcamp.notifications.signals
