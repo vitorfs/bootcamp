d3cf0b62552113b235af124f728686d40f697c0f
Sebastian Reyes <sebaslander@gmail.com>
2018-03-30 21:34:44 -0500

    Adding necessary views to mark notifications as read.

--- bootcamp/notifications/urls.py
+++ bootcamp/notifications/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
         view=views.NotificationUnreadListView.as_view(),
         name='unread'
     ),
+    url(
+        regex=r'^mark-as-read/(?P<slug>[-\w]+)/$',
+        view=views.mark_as_read,
+        name='mark_as_read'
+    ),
+    url(
+        regex=r'^mark-all-as-read/$',
+        view=views.mark_all_as_read,
+        name='mark_all_read'
+    ),
 ]
--- bootcamp/notifications/views.py
+++ bootcamp/notifications/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView  # , DetailView, RedirectView, UpdateView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView
 
 from bootcamp.notifications.models import Notification
 
@@ -13,3 +16,32 @@ class NotificationUnreadListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self, **kwargs):
         return self.request.user.notifications.unread()
+
+
+@login_required
+def mark_all_as_read(request):
+    request.user.notifications.mark_all_as_read()
+    _next = request.GET.get('next')
+    messages.add_message(request, messages.SUCCESS,
+        f'All notifications to {request.user.username} have been marked as read.')
+
+    if _next:
+        return redirect(_next)
+
+    return redirect('notifications:unread')
+
+
+@login_required
+def mark_as_read(request, slug=None):
+    if slug:
+        notification = get_object_or_404(Notification, slug=slug)
+        notification.mark_as_read()
+
+    messages.add_message(request, messages.SUCCESS,
+        f'The notification {notification.slug} has been marked as read.')
+    _next = request.GET.get('next')
+
+    if _next:
+        return redirect(_next)
+
+    return redirect('notifications:unread')
