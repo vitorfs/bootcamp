057a0ffc9a30e4b9c44a51c7ae5df4c201343c14
Sebastian Reyes <sebaslander@gmail.com>
2018-04-05 13:33:45 -0500

    Adding the proper pattern for routing application protocol.

--- config/routing.py
+++ config/routing.py
@@ -1,5 +1,9 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from bootcamp.notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
