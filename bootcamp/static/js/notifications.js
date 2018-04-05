4266c34d816ce3be04a35c413058a2943c50fab0
Sebastian Reyes <sebaslander@gmail.com>
2018-04-05 09:34:43 -0500

    At least the AJAX call is working as intended, it was hard to find the proper way to construct the popover.

--- bootcamp/static/js/notifications.js
+++ bootcamp/static/js/notifications.js
@@ -9,61 +9,25 @@ $(function () {
     $("#notifications").click(function () {
         if ($(".popover").is(":visible")) {
             $("#notifications").popover('hide');
+            $("#notifications").popover('dispose');
         }
         else {
-            $("#notifications").popover('dispose');
             $.ajax({
                 url: '/notifications/latest-notifications/',
                 cache: false,
-                beforeSend: function () {
-                    //$("#notifications").popover("dispose");
+                success: function (data) {
+                    $("#notifications").popover('dispose');
                     $("#notifications").popover({
                         html: true,
-                        trigger: 'manual',
+                        trigger: 'focus',
                         container: "body" ,
                         placement: "bottom",
-                        content: "Text",// "<div style='text-align:center'><img src='/static/img/loading.gif'></div>",
-                    }).popover("show");
-                    //$("#notifications").popover("show");
-                    console.log('This is being done!')
-                    // $(this).attr('data-content', "<div style='text-align:center'><img src='/static/img/loading.gif'></div>");
-
+                        content: data,
+                    });
+                    $("#notifications").popover('show');
                 },
-                success: function (data) {
-                    $(this).attr('data-content', data);
-                    console.log('Loaded')
-                }
             });
         }
         return false;
     });
 });
-/*
-$(function () {
-    $('#notifications').popover({
-        html: true,
-        trigger: 'manual',
-        container: "body" ,
-        placement: "bottom",
-    });
-
-    $("#notifications").click(function () {
-        if ($(".popover").is(":visible")) {
-            $("#notifications").popover('hide');
-        }
-        else {
-            $("#notifications").popover('show');
-            $.ajax({
-                url: '/notifications/latest-notifications/',
-                beforeSend: function () {
-                    $(".popover-content").html("<div style='text-align:center'><img src='/static/img/loading.gif'></div>");
-                },
-                success: function (data) {
-                    $(".popover-content").html(data);
-                }
-            });
-        }
-        return false;
-    });
-});
- */
