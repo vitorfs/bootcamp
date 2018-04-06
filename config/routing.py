df7eee44952bd35c248e2440bce87f43cfb00a85
Sebastian Reyes <sebaslander@gmail.com>
2018-04-06 11:08:48 -0500

    Adding a nice validation to avoid XSS atacks, courtesy from django-channels.

--- config/routing.py
+++ config/routing.py
@@ -1,9 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from bootcamp.notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
