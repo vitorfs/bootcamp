520d6a6c7181c992ef58e805ac271839d97822a7
Sebastian Reyes <sebaslander@gmail.com>
2018-04-06 11:10:23 -0500

    Small fix to the route.

--- bootcamp/notifications/routing.py
+++ bootcamp/notifications/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from bootcamp.notifications.consumers import NotificationsConsumer
 
 websocket_urlpatterns = [
-    url(r'^notifications/$', NotificationsConsumer),
+    url(r'^ws-notifications/$', NotificationsConsumer),
 ]
