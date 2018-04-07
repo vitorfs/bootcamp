63d1d52b80e063c7fd243c2acf859f94907b02bf
Sebastian Reyes <sebaslander@gmail.com>
2018-04-07 12:24:03 -0500

    Small change to the WS path.

--- bootcamp/notifications/routing.py
+++ bootcamp/notifications/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from bootcamp.notifications.consumers import NotificationsConsumer
 
 websocket_urlpatterns = [
-    url(r'^ws-notifications/$', NotificationsConsumer),
+    url(r'^notifications/$', NotificationsConsumer),
 ]
