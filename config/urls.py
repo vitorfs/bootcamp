5f43f22bf1a17041141e4151dea87714ba898057
Sebastian Reyes <sebaslander@gmail.com>
2018-04-10 09:05:29 -0500

    Fixing a typo and including the urls in the core URLConf.

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     # Local apps here
     url(r'^notifications/',
         include('bootcamp.notifications.urls', namespace='notifications'))
+    url(r'^articles/', include('bootcamp.articles.urls', namespace='articles')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
