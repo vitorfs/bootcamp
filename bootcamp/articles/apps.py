2da73b8de181c69739899acdf72aeb50626637b0
Sebastian Reyes <sebaslander@gmail.com>
2018-04-10 08:06:27 -0500

    Updating the model to include additional functionality, defining a basic admin and getting up to speed the apps file.

--- bootcamp/articles/apps.py
+++ bootcamp/articles/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ArticlesConfig(AppConfig):
-    name = 'articles'
+    name = 'bootcamp.articles'
+    verbose_name = _('Articles')
