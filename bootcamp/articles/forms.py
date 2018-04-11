59831ff90dbcaa5309e40bbfd748d5fcd9391892
Sebastian Reyes <sebaslander@gmail.com>
2018-04-11 15:33:04 -0500

    Small styling change.

--- bootcamp/articles/forms.py
+++ bootcamp/articles/forms.py
@@ -5,7 +5,8 @@ from bootcamp.articles.models import Article
 
 class ArticleForm(forms.ModelForm):
     status = forms.CharField(widget=forms.HiddenInput())
-    edited = forms.BooleanField(widget=forms.HiddenInput(), required=False, initial=False)
+    edited = forms.BooleanField(
+        widget=forms.HiddenInput(), required=False, initial=False)
 
     class Meta:
         model = Article
