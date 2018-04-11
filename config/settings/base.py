41f8ac33fde828ba2cc8c9d18f5c27c602063e0a
Sebastian Reyes <sebaslander@gmail.com>
2018-04-11 12:32:31 -0500

    Humanize this.

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ DJANGO_APPS = [
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.humanize', # Handy template tags
+    'django.contrib.humanize',
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
