f5e55c4f932d22874cb03b6a0b466b399e9dec06
Sebastian Reyes <sebaslander@gmail.com>
2018-04-12 13:23:46 -0500

    Fixing installed apps and including the new one.

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,6 +83,7 @@ LOCAL_APPS = [
     'bootcamp.activities.apps.ActivitiesConfig',
     'bootcamp.notifications.apps.NotificationsConfig',
     'bootcamp.articles.apps.ArticlesConfig',
+    'bootcamp.news.apps.NewsConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/settings/local.py
+++ config/settings/local.py
@@ -62,7 +62,7 @@ if os.environ.get('USE_DOCKER') == 'yes':
 # django-extensions
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-INSTALLED_APPS += ['extensions']  # noqa F405
+INSTALLED_APPS += ['django_extensions']  # noqa F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
