b7877c138397c6991d6b297e191aaff226af1002
Sebastian Reyes <sebaslander@gmail.com>
2018-04-18 16:35:23 -0500

    Provinding a more coherent name.

--- bootcamp/news/urls.py
+++ bootcamp/news/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.NewsListView.as_view(), name='list'),
     url(r'^delete/(?P<pk>[-\w]+)/$',
         views.NewsDeleteView.as_view(), name='delete_news'),
-    url(r'^post/$', views.post, name='post_new'),
+    url(r'^post-news/$', views.post_news, name='post_news'),
     url(r'^like/$', views.like, name='like_post'),
     url(r'^get-comments/$', views.get_news_comments, name='get_comments'),
 ]
