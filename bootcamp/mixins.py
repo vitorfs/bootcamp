12fbe7885d753e231aa18d8d8052888e2eee1f57
Sebastian Reyes <sebaslander@gmail.com>
2018-04-17 20:37:24 -0500

    Adding not a mixin but a decorator, perhaps changing the name to helpers would make sense.

--- bootcamp/mixins.py
+++ bootcamp/mixins.py
@@ -1,9 +1,24 @@
-from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseBadRequest
 from django.views.generic import View
 
 
+def ajax_required(f):
+    """Not a mixin, but a nice decorator to validate than a request is AJAX"""
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+
+        return f(request, *args, **kwargs)
+
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
+    return wrap
+
+
 class AuthorRequiredMixin(View):
+    """Mixin to validate than the loggedin user is the creator of the object
+    to be edited or updated."""
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
         if obj.user != self.request.user:
