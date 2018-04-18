e1a172b8248a4b1d0c0f413b0a96973994fe273a
Sebastian Reyes <sebaslander@gmail.com>
2018-04-17 22:04:51 -0500

    Adding a DeleteView with POST confirmation.

--- bootcamp/news/views.py
+++ bootcamp/news/views.py
@@ -6,21 +6,31 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.template.context_processors import csrf
 from django.template.loader import render_to_string
+from django.urls import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 
 from bootcamp.news.models import News
-from bootcamp.mixins import ajax_required
+from bootcamp.mixins import ajax_required, AuthorRequiredMixin
 
 
 class NewsListView(LoginRequiredMixin, ListView):
+    """A really simple ListView, with some JS magic on the UI."""
     model = News
     paginate_by = 15
+
+
+class NewsDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
+    """Implementation of the DeleteView overriding the delete method to
+    allow a no-redirect response to use with AJAX call."""
+    model = News
+    success_url = reverse_lazy("news:list")
 
 
 @login_required
 @ajax_required
 def post(request):
+    """A function view to implement the post functionality with AJAX."""
     user = request.user
     csrf_token = (csrf(request)['csrf_token'])
     post = request.POST['post']
