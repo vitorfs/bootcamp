044380016fed8e5d1d1b7188d1dc20810cd20e50
Sebastian Reyes <sebaslander@gmail.com>
2018-04-17 20:38:02 -0500

    Keeping the views who work and their respective URL.

--- bootcamp/news/urls.py
+++ bootcamp/news/urls.py
@@ -5,4 +5,5 @@ from bootcamp.news import views
 app_name = 'news'
 urlpatterns = [
     url(r'^$', views.NewsListView.as_view(), name='list'),
+    url(r'^post/$', views.post, name='post_new'),
 ]
--- bootcamp/news/views.py
+++ bootcamp/news/views.py
@@ -1,19 +1,42 @@
+import json
+
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.template.context_processors import csrf
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import ListView
 
 from bootcamp.news.models import News
-
-
-class NewsCreateView(LoginRequiredMixin, CreateView):
-    model = News
-    success_url = reverse_lazy("news:list")
-
-
-class NewsDetailView(LoginRequiredMixin, DetailView):
-    model = News
+from bootcamp.mixins import ajax_required
 
 
 class NewsListView(LoginRequiredMixin, ListView):
     model = News
     paginate_by = 15
+
+
+@login_required
+@ajax_required
+def post(request):
+    user = request.user
+    csrf_token = (csrf(request)['csrf_token'])
+    post = request.POST['post']
+    post = post.strip()
+    if len(post) > 0 and len(post) <= 280:
+        posted = News.objects.create(
+            user=user,
+            content=post,
+        )
+        html = render_to_string(
+            'news/partial_news.html',
+            {
+                'news': posted,
+                'csrf_token': csrf_token,
+                'request': request
+            })
+        return HttpResponse(html)
+
+    else:
+        return HttpResponseBadRequest()
