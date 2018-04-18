b2110eab8627c26c07ab775a41c1c42890f2cc0a
Sebastian Reyes <sebaslander@gmail.com>
2018-04-17 22:04:51 -0500

    Adding a DeleteView with POST confirmation.

--- bootcamp/news/urls.py
+++ bootcamp/news/urls.py
@@ -5,5 +5,7 @@ from bootcamp.news import views
 app_name = 'news'
 urlpatterns = [
     url(r'^$', views.NewsListView.as_view(), name='list'),
+    url(r'^delete/(?P<pk>[-\w]+)/$',
+        views.NewsDeleteView.as_view(), name='delete_news'),
     url(r'^post/$', views.post, name='post_new'),
 ]
