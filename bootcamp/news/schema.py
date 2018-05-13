84058f6abed85b46ae13f860b9db4dfccb0f8355
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-05-12 20:02:13 -0500

    Improving accesability.

--- bootcamp/news/schema.py
+++ bootcamp/news/schema.py
@@ -13,6 +13,15 @@ class NewsType(DjangoObjectType):
 
 class NewsQuery(object):
     all_news = graphene.List(NewsType)
+    news = graphene.Field(NewsType, uuid_id=graphene.String())
 
     def resolve_all_news(self, info, **kwargs):
         return News.objects.all()
+
+    def resolve_news(self, info, **kwargs):
+        uuid_id = kwargs.get('uuid_id')
+
+        if uuid_id is not None:
+            return News.objects.get(uuid_id=uuid_id)
+
+        return None
