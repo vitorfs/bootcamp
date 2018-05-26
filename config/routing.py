bf241a0af8065f64de65ccbb5808d2c065c584b1
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-05-26 07:46:10 -0500

    Added django-channels consumers and the required routing. Deleted the previous routing structure to allow multiple consumers. Added messager model method to communicate to the WebSocket. Added JS client functionlity and include them all in the template.

--- config/routing.py
+++ config/routing.py
@@ -1,11 +1,21 @@
+from django.conf.urls import url
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from bootcamp.notifications.routing import websocket_urlpatterns
+from bootcamp.messager.consumers import MessagerConsumer
+from bootcamp.notifications.consumers import NotificationsConsumer
+# from bootcamp.notifications.routing import notifications_urlpatterns
+# from bootcamp.messager.routing import messager_urlpatterns
 
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        AuthMiddlewareStack(
+            URLRouter([
+                url(r'^notifications/$', NotificationsConsumer),
+                url(r'^(?P<username>[^/]+)/$', MessagerConsumer),
+            ])
+        ),
     ),
 })
