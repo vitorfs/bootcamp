81be20721a1e52f70e0773c8d2cd1c1814e91064
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-06-12 16:46:30 -0500

    Lets vote on this questions... Questions have voting feature now.

--- bootcamp/qa/views.py
+++ bootcamp/qa/views.py
@@ -1,14 +1,15 @@
+from django.db.utils import IntegrityError
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.urls import reverse
 from django.utils.translation import ugettext as _
 from django.views.generic import CreateView, ListView, DetailView
 
 from bootcamp.helpers import ajax_required
-from bootcamp.qa.models import Question, Answer
+from bootcamp.qa.models import Question, Answer, Vote
 from bootcamp.qa.forms import QuestionForm
 
 
@@ -61,3 +62,33 @@ class CreateAnswerView(LoginRequiredMixin, CreateView):
         messages.success(self.request, self.message)
         return reverse(
             "qa:question_detail", kwargs={"pk": self.kwargs["question_id"]})
+
+
+@login_required
+@ajax_required
+def question_vote(request):
+    """Function view to receive AJAX call, returns the count of votes a given
+    question has recieved."""
+    if request.method == "POST":
+        question_id = request.POST["question"]
+        value = None
+        if request.POST["value"] == "U":
+            value = True
+
+        else:
+            value = False
+
+        question = Question.objects.get(pk=question_id)
+        try:
+            question.votes.update_or_create(
+                user=request.user, defaults={"value": value}, )
+            question.count_votes()
+            return JsonResponse({"votes": question.total_votes})
+
+        except IntegrityError:
+            return JsonResponse({'status': 'false',
+                                 'message': _("Database integrity error.")},
+                                status=500)
+
+    else:
+        return HttpResponseBadRequest(content=_("Wrong request type."))
