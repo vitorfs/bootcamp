d8f22f1dbec68b573d046f11c0669ac4b376c25a
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-06-17 18:31:53 -0500

    Let's test this thing.

--- bootcamp/users/tests/test_models.py
+++ bootcamp/users/tests/test_models.py
@@ -9,11 +9,16 @@ class TestUser(TestCase):
     def test__str__(self):
         self.assertEqual(
             self.user.__str__(),
-            'testuser'  # This is the default username for self.make_user()
+            "testuser"  # This is the default username for self.make_user()
         )
 
     def test_get_absolute_url(self):
         self.assertEqual(
             self.user.get_absolute_url(),
-            '/users/testuser/'
+            "/users/testuser/"
         )
+
+    def test_get_profile_name(self):
+        assert self.user.get_profile_name() == "testuser"
+        self.user.name = "Test User"
+        assert self.user.get_profile_name() == "Test User"
