90eb789d4fa8205d59069507ce84a98b6a564937
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-07-02 08:20:34 -0500

    Fixing typos.

--- bootcamp/messager/tests/test_models.py
+++ bootcamp/messager/tests/test_models.py
@@ -3,7 +3,7 @@ from test_plus.test import TestCase
 from bootcamp.messager.models import Message
 
 
-class QAModelsTest(TestCase):
+class MessagerModelsTest(TestCase):
     def setUp(self):
         self.user = self.make_user("test_user")
         self.other_user = self.make_user("other_test_user")
