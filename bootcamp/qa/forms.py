f4436af9e758c502fe11c588a7eeb9bc1eb7210e
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-07-03 20:37:24 -0500

    Adding the markdownx app to the required templates, and overriding the widget for better aspect.

--- bootcamp/qa/forms.py
+++ bootcamp/qa/forms.py
@@ -1,23 +1,14 @@
 from django import forms
 
+from markdownx.fields import MarkdownxFormField
+
 from bootcamp.qa.models import Answer, Question
 
 
 class QuestionForm(forms.ModelForm):
     status = forms.CharField(widget=forms.HiddenInput())
+    content = MarkdownxFormField()
 
     class Meta:
         model = Question
         fields = ["title", "content", "tags", "status"]
-
-
-class AnswerForm(forms.ModelForm):
-    question = forms.ModelChoiceField(widget=forms.HiddenInput(),
-                                      queryset=Question.objects.all())
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}),
-        max_length=2000)
-
-    class Meta:
-        model = Answer
-        fields = ["question", "description"]
