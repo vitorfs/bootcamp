e4c4b17f1d17e8cd536f3ecca3d97328d0816b6c
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-12-07 07:42:30 -0500

    Updating setup to consolidate elements location.

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,7 +173,7 @@ STATICFILES_FINDERS = [
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-MEDIA_ROOT = str(APPS_DIR('media'))
+MEDIA_ROOT = str(ROOT_DIR('media'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/media/'
 
