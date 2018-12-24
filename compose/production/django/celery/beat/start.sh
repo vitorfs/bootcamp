02df9ed42eaa4c3c795ca63bf631ab543811f1f1
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2018-12-24 06:54:30 -0500

    Removing Celery related content and files because it was not in use.

--- compose/production/django/celery/beat/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-
-celery -A bootcamp.taskapp beat -l INFO
--- config/settings/base.py
+++ config/settings/base.py
@@ -12,8 +12,8 @@ env.read_env(str(ROOT_DIR.path('.env')))
 
 # READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
 # if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    # env.read_env(str(ROOT_DIR.path('.env')))
+# OS environment variables take precedence over variables from .env
+# env.read_env(str(ROOT_DIR.path('.env')))
 
 # GENERAL
 # ------------------------------------------------------------------------------
