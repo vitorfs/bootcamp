b5c5d2fa34310d3dc80abcf11127767210eb625e
gusbakker <gustavobakker@hotmail.com>
2020-04-22 02:19:13 +0100

    removed .env variable for heroku production settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,12 +10,12 @@ ROOT_DIR = (
 APPS_DIR = ROOT_DIR.path("bootcamp")
 
 env = environ.Env()
-env.read_env(str(ROOT_DIR.path(".env")))
+# env.read_env(str(ROOT_DIR.path(".env")))
 
-# READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
-# if READ_DOT_ENV_FILE:
-# OS environment variables take precedence over variables from .env
-# env.read_env(str(ROOT_DIR.path('.env')))
+READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
+if READ_DOT_ENV_FILE:
+    # OS environment variables take precedence over variables from .env
+    env.read_env(str(ROOT_DIR.path('.env')))
 
 # GENERAL
 # ------------------------------------------------------------------------------
