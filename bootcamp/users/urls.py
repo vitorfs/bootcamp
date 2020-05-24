513cedb1912e668c689026ffc73eb657e9c5e209
gusbakker <gustavobakker@hotmail.com>
2020-05-24 03:17:00 +0100

    Fixed Contacts. The url to see all contacts is now fixed

--- bootcamp/users/urls.py
+++ bootcamp/users/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    url(regex=r"^$", view=views.UserListView.as_view(), name="list"),
+    url(regex=r"^~all/$", view=views.UserListView.as_view(), name="list"),
     url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
     url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
     url(
