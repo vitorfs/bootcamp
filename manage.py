6b645ebfb6c377c92d263d74344b5e1b182b2940
Matt Trainer <mmiinnovations@gmail.com>
2020-06-06 06:28:48 -0700

    mogul1

--- manage.py
+++ manage.py
@@ -2,29 +2,29 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-
-    try:
-        from django.core.management import execute_from_command_line
-
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions.
-        try:
-            import django  # noqa
-
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-
-        raise
-
-    # This allows easy placement of apps within the inner bootcamp directory.
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "bootcamp"))
-
-    execute_from_command_line(sys.argv)
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+	
+	try:
+		from django.core.management import execute_from_command_line
+	
+	except ImportError:
+		# The above import may fail for some other reason. Ensure that the
+		# issue is really that Django is missing to avoid masking other
+		# exceptions.
+		try:
+			import django  # noqa
+		
+		except ImportError:
+			raise ImportError(
+				"Couldn't import Django. Are you sure it's installed and "
+				"available on your PYTHONPATH environment variable? Did you "
+				"forget to activate a virtual environment?"
+			)
+		
+		raise
+	
+	# This allows easy placement of apps within the inner bootcamp directory.
+	current_path = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(os.path.join(current_path, "bootcamp"))
+	
+	execute_from_command_line(sys.argv)
