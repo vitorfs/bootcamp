b357cb88bd66a4abaabec8f35d1e64537470d93e
marek <mowsianowski@milosolutions.com>
2020-06-26 17:39:48 +0200

    Add tag list view. Refactor getting tags with usage count. Implement taggit-selectize.

--- bootcamp/articles/urls.py
+++ bootcamp/articles/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from bootcamp.articles.views import (
     ArticlesListView,
+    ArticlesByTagListView,
     DraftsListView,
     CreateArticleView,
     EditArticleView,
@@ -11,6 +12,7 @@ from bootcamp.articles.views import (
 app_name = "articles"
 urlpatterns = [
     url(r"^$", ArticlesListView.as_view(), name="list"),
+    url(r"^tag/(?P<slug>[-\w]+)/$", ArticlesByTagListView.as_view(), name="by_tag_list"),
     url(r"^write-new-article/$", CreateArticleView.as_view(), name="write_new"),
     url(r"^drafts/$", DraftsListView.as_view(), name="drafts"),
     url(r"^edit/(?P<pk>\d+)/$", EditArticleView.as_view(), name="edit_article"),
